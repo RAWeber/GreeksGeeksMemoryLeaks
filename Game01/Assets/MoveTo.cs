00c741806e5540270a736bd255144cccf58d3206
Robert Weber <RAWeber95@gmail.com>
2016-06-12 12:16:57 -0700

    Adding Unity Project
    Testing functionality of Github with Unity

---
+++ Game01/Assets/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+    public Transform goal;
+    [Range(1,3)]
+    public int path;    //1 - Left, 2 - Center, 3 - Right
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //Uses bitvavalues for navmesh path
+        agent.areaMask = 4 + 8 * ((int)Mathf.Pow(2, path - 1)) + 64;
+    }
+    
+    void Start()
+    {
+        agent.destination = goal.position;
+    }
+}
